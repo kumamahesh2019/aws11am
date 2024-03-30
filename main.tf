6458d5a46ed04fd578ca5366b06a38eef5453860
kumamahesh2019 <164604202+kumamahesh2019@users.noreply.github.com>
2024-03-30 16:23:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,5 @@
 provider "aws" {
-  access_key = "fhhgkkhkhklhkhkdkdkd"
-  secret_key = "hjsklkrkrrjkkjfkkvklvk"
+  access_key = "AKIA3FLD2FARKSF3OLX5"
+  secret_key = "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"
+  region = "us-east-1"
 }
